9ac345ac7bb9fa1cbb9bca6edc26743d913b7352
lyfetech <aliefgilang54@gmail.com>
2023-03-14 06:18:08 +0700

    power.c minor revision

--- Strukdat/functions/power.c
+++ Strukdat/functions/power.c
@@ -2,12 +2,12 @@
 #include<stdlib.h>
 #include<stdbool.h>
 
-long double powie(long long a, long long b){
+long double powie(long double a, long long b){
     long double res=1.0;
     bool y = b>0 ? 0 : 1 ;
     b=llabs(b);
     while(b>0){
-        if(b&1) res*=(long double)a;
+        if(b&1) res*=a;
         a*=a;
         b>>=1;
     }
@@ -16,11 +16,12 @@ long double powie(long long a, long long b){
 }
 
 int main(){
-    long long x,y;
+    long long y;
+    long double x;
     printf("Program perpangkatan X^Y\n");
     printf("Masukkan nilai X^Y (format : X Y) : ");
-    scanf("%lld %lld", &x, &y);
-    printf("Hasil dari %lld^%lld = %Lg\n", x,y,powie(x,y));
+    scanf("%Lf %lld", &x, &y);
+    printf("Hasil dari %Lg^%lld = %Lg\n", x,y,powie(x,y));
 }
 
 
